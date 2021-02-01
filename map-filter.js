cfe538f1d0d29b815e315cea5b97eccdcc3df5ad
Jubair Saad <jubairsaad2018@gmail.com>
2021-02-02 00:16:31 +0600

    hi

---
+++ map-filter.js
@@ -0,0 +1,7 @@
+const numbers = [3,5,6,7,8,9];
+// const output = [];
+// for (let i = 0 ; i<numbers.length; i ++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result) ;
+// }console.log(output);
---
+++ object-property.js
@@ -0,0 +1,8 @@
+const students = [
+    {id : 23, name : "akbbv"},
+    {id : 53, name : "Saad"},
+    {id : 43, name : "Jubairv"},
+    {id : 63, name : "Akib"}
+];
+const Names = students.map (s => s.name);
+console.log(Names);
